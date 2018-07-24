88aa7d120559991da4312dc23227d584c23867f0
obi136 <obi136@gmail.com>
2018-07-24 13:09:01 -0400

    final changes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,5 @@
 var friends = [{
-    name: "Ahmed",
+    name: "Darth Ahmed",
     photo: "https://heroichollywood.b-cdn.net/wp-content/uploads/2017/03/Star-Wars-Rogue-On-Riz-Ahmed-Bodhi-Rook-1024x512.jpg",
     scores: [
         5,
@@ -12,6 +12,74 @@ var friends = [{
         5,
         4,
         1
+    ]
+},
+
+{
+    name: "Darth Sidious",
+    photo: "https://lumiere-a.akamaihd.net/v1/images/open-uri20150608-27674-zgiura_6090be71.jpeg?region=0%2C0%2C1280%2C720",
+    scores: [
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5
+    ]
+},
+
+{
+    name: "Darth Vader",
+    photo: "https://www.sideshowtoy.com/wp-content/uploads/2014/07/star-wars-darth-vader-life-size-feature-400184.jpg",
+    scores: [
+        3,
+        5,
+        2,
+        2,
+        4,
+        5,
+        3,
+        4,
+        5,
+        5
+    ]
+},
+
+{
+    name: "Count Dooku",
+    photo: "https://vignette.wikia.nocookie.net/antagonists/images/1/18/Count_dookus.jpg/revision/latest?cb=20130703175436",
+    scores: [
+        4,
+        4,
+        4,
+        4,
+        4,
+        4,
+        4,
+        4,
+        4,
+        4
+    ]
+},
+
+{
+    name: "Asajj Ventress",
+    photo: "https://i.pinimg.com/originals/2f/3a/59/2f3a591f18e18a89dc1a8184f62f9e50.png",
+    scores: [
+        2,
+        5,
+        5,
+        5,
+        3,
+        3,
+        2,
+        2,
+        1,
+        5
     ]
 }];
 
